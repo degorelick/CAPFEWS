7765c75a0a5de186fca5768b437bb516ea869dfb
degorelick <degorelick@gmail.com>
2022-11-03 22:37:20 -0400

    include R script to parse hdf5 CAPFEWS output and return a csv

---
+++ results/explore_CAPFEWS_output.R
@@ -0,0 +1,25 @@
+### -----------------------------------------------------
+##  Explore CAPFEWS Results
+##    D. Gorelick (Nov 2022)
+##  To be stored in degorelick/CAPFEWS repository on GitHub
+### -----------------------------------------------------
+
+rm(list=ls()) # clear memory
+setwd('C:/Users/dgorelic/OneDrive - University of North Carolina at Chapel Hill/UNC/Research/IM3/CAP/CAPFEWS/results') # set directory
+library(tidyverse)
+
+# read output from hdf5 file
+# to do this, run: install.packages("BiocManager")
+# then run: BiocManager::install("rhdf5")
+library(rhdf5)
+CAPFEWS_names = h5readAttributes("results.hdf5", name = "s", native = TRUE)
+CAPFEWS_colnames = c(CAPFEWS_names$columns0, CAPFEWS_names$columns1, CAPFEWS_names$columns2)
+h5closeAll()
+
+CAPFEWS_output = H5Fopen("results.hdf5", native = TRUE)
+CO = as.data.frame(CAPFEWS_output$s)
+colnames(CO) = CAPFEWS_colnames
+CO$month_of_simulation = c(1:nrow(CO))
+
+# export as csv
+write.table(CO, "results.csv", sep = ",", row.names = FALSE, col.names = TRUE)
